9e24b995e9136c8b4be689c97bf1e11dad9836fb
Gan Ze Yu <52880557+zeyugan@users.noreply.github.com>
2024-05-12 14:29:20 +0800

    Update contract.ts

--- voting-backend/src/contract.ts
+++ voting-backend/src/contract.ts
@@ -1,18 +1,110 @@
-// Find all our documentation at https://docs.near.org
-import { NearBindgen, near, call, view } from 'near-sdk-js';
+import {
+  NearBindgen,
+  near,
+  call,
+  view,
+  LookupMap,
+  UnorderedSet,
+  UnorderedMap,
+} from "near-sdk-js";
 
 @NearBindgen({})
-class HelloNear {
-  greeting: string = 'Hello';
+class VotingContract {
+  // Candidate Pair Used to store Candidate Names and URL links
+  candidatePair = new UnorderedMap<string[]>("candidate_pair");
+  // Prompt Set Was used to in an effort to keep track of keys for the candidatePair Unordered Map
+  promptSet = new UnorderedSet<string>("promptArray");
+  voteArray = new UnorderedMap<number[]>("voteArray");
+  userParticipation = new UnorderedMap<string[]>("user Participation ");
 
-  @view({}) // This method is read-only and can be called for free
-  get_greeting(): string {
-    return this.greeting;
+  // Writing View Methods
+
+  @view({})
+  getUrl({ prompt, name }: { prompt: string; name: string }): string {
+    near.log(prompt);
+    let candidateUrlArray = this.candidatePair.get(prompt);
+    return candidateUrlArray[candidateUrlArray.indexOf(name) + 1];
   }
 
-  @call({}) // This method changes the state, for which it cost gas
-  set_greeting({ greeting }: { greeting: string }): void {
-    near.log(`Saving greeting ${greeting}`);
-    this.greeting = greeting;
+  @view({})
+  didParticipate({ prompt, user }: { prompt: string; user: string }): boolean {
+    let promptUserList: string[] = this.userParticipation.get(prompt, {
+      defaultValue: [],
+    });
+    near.log(promptUserList);
+    return promptUserList.includes(user);
+  }
+
+  @view({})
+  participateArray({ prompt }: { prompt: string }): string[] {
+    return this.userParticipation.get(prompt);
+  }
+
+  @view({})
+  getAllPrompts(): string[] {
+    return this.promptSet.toArray();
+  }
+
+  @view({})
+  getVotes({ prompt }: { prompt: string }): number[] {
+    return this.voteArray.get(prompt, { defaultValue: [] });
+  }
+
+  @view({})
+  getCandidatePair({ prompt }: { prompt: string }): string[] {
+    let candidateUrlArray = this.candidatePair.get(prompt, {
+      defaultValue: ["n/a,n/a"],
+    });
+    return [candidateUrlArray[0], candidateUrlArray[2]];
+  }
+
+  @call({})
+  addCandidatePair({
+    prompt,
+    name1,
+    name2,
+    url1,
+    url2,
+  }: {
+    prompt: string;
+    name1: string;
+    name2: string;
+    url1: string;
+    url2: string;
+  }) {
+    this.candidatePair.set(prompt, [name1, url1, name2, url2]);
+  }
+
+  @call({})
+  initializeVotes({ prompt }: { prompt: string }) {
+    this.voteArray.set(prompt, [0, 0]);
+  }
+
+  @call({})
+  addToPromptArray({ prompt }: { prompt: string }) {
+    this.promptSet.set(prompt);
+  }
+
+  @call({})
+  clearPromptArray() {
+    this.promptSet.clear();
+    this.candidatePair.clear();
+    this.userParticipation.clear();
+    this.voteArray.clear();
+    near.log("clearing polls");
+  }
+
+  @call({})
+  addVote({ prompt, index }: { prompt: string; index: number }) {
+    let currentVotes = this.voteArray.get(prompt, { defaultValue: [0, 0] });
+    currentVotes[index] = currentVotes[index] + 1;
+    this.voteArray.set(prompt, currentVotes);
+  }
+
+  @call({})
+  recordUser({ prompt, user }: { prompt: string; user: string }) {
+    let currentArray = this.userParticipation.get(prompt, { defaultValue: [] });
+    currentArray.includes(user) ? null : currentArray.push(user);
+    this.userParticipation.set(prompt, currentArray);
   }
 }
